c8fa2f33b54c06a2dea3bf3f7b0cda6fa6722480
Christian Helle <christian.helle@outlook.com>
2025-06-18 21:48:57 +0200

    Remove version_info.zig and ignore version_info as this is generated at build time

--- src/version_info.zig
+++
@@ -1,5 +0,0 @@
-// This file is auto-generated at build time
-pub const VERSION = "1.0.0";
-pub const GIT_TAG = "v0.1.5-16-gb38da99-dirty";
-pub const GIT_COMMIT = "b38da99";
-pub const BUILD_DATE = "2025-06-18 12.19.36 UTC";
