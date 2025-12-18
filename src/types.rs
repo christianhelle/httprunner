d2880254837b906dfa1bf592b1ced8fc76dd1676
Christian Helle <christian.helle@outlook.com>
2025-12-18 17:55:24 +0100

    Introduce the types HttpFileResults and ProcessorResults

--- src/types.rs
+++ src/types.rs
@@ -103,3 +103,18 @@ pub struct RequestContext {
     pub request: HttpRequest,
     pub result: Option<HttpResult>,
 }
+
+#[derive(Debug, Clone)]
+pub struct HttpFileResults {
+    pub filename: String,
+    pub success_count: u32,
+    pub failed_count: u32,
+    pub skipepd_count: u32,
+    pub result_contexts: Vec<RequestContext>,
+}
+
+#[derive(Debug, Clone)]
+pub struct ProcessorResults {
+    pub success: bool,
+    pub files: Vec<HttpFileResults>,
+}
