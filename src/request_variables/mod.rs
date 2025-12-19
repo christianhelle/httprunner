089c5c8f6377bf9b832b0cc96ebba54c59f20697
Christian Helle <christian.helle@outlook.com>
2025-12-19 15:53:26 +0100

    Fix unused import warnings

--- src/request_variables/mod.rs
+++ src/request_variables/mod.rs
@@ -3,12 +3,12 @@ mod json;
 mod parser;
 mod substitution;
 
+#[allow(unused_imports)]
 pub use extractor::extract_request_variable_value;
 pub use json::extract_json_property;
-pub use substitution::substitute_request_variables;
-
 #[allow(unused_imports)]
 pub use parser::parse_request_variable;
+pub use substitution::substitute_request_variables;
 
 #[cfg(test)]
 mod tests;
