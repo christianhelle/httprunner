918e57f8d1750f08a73be740779fba99fd1e0ddb
Christian Helle <christian.helle@outlook.com>
2026-01-08 15:38:17 +0100

    Add Base64EncodeSubstitutor for `base64_encode()` function substitution
    Implemented `Base64EncodeSubstitutor` to handle `base64_encode()` substitutions. Updated function substitution logic and `.http` example to demonstrate usage. Added tests for `base64_encode()` and included `base64` crate as a dependency.

--- src/functions/generator.rs
+++ src/functions/generator.rs
@@ -1,9 +1,10 @@
 use crate::functions::substitution::FunctionSubstitutor;
+use regex::RegexBuilder;
 
 pub struct GuidSubstitutor {}
 impl FunctionSubstitutor for GuidSubstitutor {
-    fn get_regex(&self) -> String {
-        r"\bguid\(\)".to_string()
+    fn get_regex(&self) -> &str {
+        r"\bguid\(\)"
     }
 
     fn generate(&self) -> String {
@@ -14,8 +15,8 @@ impl FunctionSubstitutor for GuidSubstitutor {
 
 pub struct StringSubstitutor {}
 impl FunctionSubstitutor for StringSubstitutor {
-    fn get_regex(&self) -> String {
-        r"\bstring\(\)".to_string()
+    fn get_regex(&self) -> &str {
+        r"\bstring\(\)"
     }
 
     fn generate(&self) -> String {
@@ -32,8 +33,8 @@ impl FunctionSubstitutor for StringSubstitutor {
 
 pub struct NumberSubstitutor {}
 impl FunctionSubstitutor for NumberSubstitutor {
-    fn get_regex(&self) -> String {
-        r"\bnumber\(\)".to_string()
+    fn get_regex(&self) -> &str {
+        r"\bnumber\(\)"
     }
 
     fn generate(&self) -> String {
@@ -43,5 +44,31 @@ impl FunctionSubstitutor for NumberSubstitutor {
         rand::rng()
             .sample(Uniform::new_inclusive(0, 100).unwrap())
             .to_string()
+    }
+}
+
+pub struct Base64EncodeSubstitutor {}
+impl FunctionSubstitutor for Base64EncodeSubstitutor {
+    fn get_regex(&self) -> &str {
+        panic!("Base64Substitutor.get_regex should not be called directly");
+    }
+
+    fn generate(&self) -> String {
+        panic!("Base64Substitutor.generate should not be called directly");
+    }
+
+    fn replace(&self, input: &String) -> String {
+        use base64::Engine;
+        use base64::engine::general_purpose;
+
+        let re = RegexBuilder::new(r"\bbase64_encode\(\s*'([^']*)'\s*\)")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+        re.replace_all(&input, |caps: &regex::Captures| {
+            let to_encode = &caps[1];
+            general_purpose::STANDARD.encode(to_encode)
+        })
+        .to_string()
     }
 }
--- src/functions/tests.rs
+++ src/functions/tests.rs
@@ -1,4 +1,6 @@
-use crate::functions::generator::{GuidSubstitutor, NumberSubstitutor, StringSubstitutor};
+use crate::functions::generator::{
+    Base64EncodeSubstitutor, GuidSubstitutor, NumberSubstitutor, StringSubstitutor,
+};
 use crate::functions::substitution::FunctionSubstitutor;
 
 #[test]
@@ -14,4 +16,12 @@ fn test_generate_string() {
 #[test]
 fn test_generate_number() {
     assert_ne!(NumberSubstitutor {}.generate(), "-1");
+}
+
+#[test]
+fn test_base64_encode() {
+    assert_eq!(
+        Base64EncodeSubstitutor {}.replace(&String::from("base64_encode('Hello, World!')")),
+        "SGVsbG8sIFdvcmxkIQ=="
+    );
 }
