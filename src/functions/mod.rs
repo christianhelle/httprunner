b08495cb67151eb582716f17282697ffb8e900a3
Christian Helle <christian.helle@outlook.com>
2026-01-08 14:43:14 +0100

    Refactor generator functions using traits
    Replaced standalone `generate_guid` and `generate_string` functions with `GuidSubstitutor` and `StringSubstitutor` structs implementing the `FunctionSubstitutor` trait. Updated function substitution logic to use substitutors dynamically.

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,9 +1,6 @@
 mod generator;
 mod substitution;
 
-pub use generator::generate_guid;
-pub use generator::generate_string;
-
 pub use substitution::substitute_functions;
 
 #[cfg(test)]
--- src/functions/substitution.rs
+++ src/functions/substitution.rs
@@ -1,15 +1,28 @@
-use super::*;
 use crate::error::Result;
+use crate::functions::generator::{GuidSubstitutor, StringSubstitutor};
 use regex::Regex;
 
-pub fn substitute_functions(input: &str) -> Result<String> {
-    let guid_regex = Regex::new(r"\bguid\(\)").unwrap();
-    let string_regex = Regex::new(r"\bstring\(\)").unwrap();
+pub trait FunctionSubstitutor {
+    fn get_regex(&self) -> String;
+    fn generate(&self) -> String;
+    fn replace(&self, input: &String) -> String {
+        Regex::new(&self.get_regex())
+            .unwrap()
+            .replace_all(&input, &self.generate())
+            .to_string()
+    }
+}
 
-    let result = guid_regex.replace_all(&input, &generate_guid()).to_string();
-    let result = string_regex
-        .replace_all(&result, &generate_string(10))
-        .to_string();
+pub fn substitute_functions(input: &str) -> Result<String> {
+    let vec: Vec<Box<dyn FunctionSubstitutor>> = vec![
+        Box::new(GuidSubstitutor {}),
+        Box::new(StringSubstitutor {}),
+    ];
+
+    let mut result = input.to_string();
+    for substitutor in vec {
+        result = substitutor.replace(&result);
+    }
 
     Ok(result)
 }
