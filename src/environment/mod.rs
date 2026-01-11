0fc1dddc7543d8b6e2c42848e09ff7f2d9220293
Christian Helle <christian.helle@outlook.com>
2026-01-11 22:08:03 +0100

    Add #[allow(unused_imports)] for GUI-related exports in environment module

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -3,6 +3,7 @@ mod loader;
 pub use loader::load_environment_file;
 
 // Export for GUI use
+#[allow(unused_imports)]
 pub use loader::{find_environment_file, parse_environment_file};
 
 #[cfg(test)]
