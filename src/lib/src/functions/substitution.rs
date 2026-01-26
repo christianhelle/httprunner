52adbf26f4a1079a1a1a9a1cf8f5b8d762da4848
Christian Helle <christian.helle@outlook.com>
2026-01-26 22:47:42 +0100

    Introduce upper() function

--- src/lib/src/functions/substitution.rs
+++ src/lib/src/functions/substitution.rs
@@ -2,11 +2,11 @@ use crate::functions::date_functions::{
     GetDateSubstitutor, GetDateTimeSubstitutor, GetTimeSubstitutor, GetUtcDateTimeSubstitutor,
 };
 use crate::functions::generator_functions::{
-    AddressSubstitutor, EmailSubstitutor, FirstNameSubstitutor,
-    GuidSubstitutor, JobTitleSubstitutor, LastNameSubstitutor, NameSubstitutor, NumberSubstitutor,
+    AddressSubstitutor, EmailSubstitutor, FirstNameSubstitutor, GuidSubstitutor,
+    JobTitleSubstitutor, LastNameSubstitutor, NameSubstitutor, NumberSubstitutor,
     StringSubstitutor,
 };
-use crate::functions::transform_functions::Base64EncodeSubstitutor;
+use crate::functions::transform_functions::{Base64EncodeSubstitutor, UpperSubstitutor};
 use anyhow::Result;
 use regex::RegexBuilder;
 
@@ -29,6 +29,7 @@ pub fn substitute_functions(input: &str) -> Result<String> {
         &StringSubstitutor {},
         &NumberSubstitutor {},
         &Base64EncodeSubstitutor {},
+        &UpperSubstitutor {},
         &NameSubstitutor {},
         &FirstNameSubstitutor {},
         &LastNameSubstitutor {},
